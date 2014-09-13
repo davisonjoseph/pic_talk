adb5fe3883a03a44c6ee5cfc9dab27aaddeca46a
Davison Joseph <davisonjoseph@gmail.com>
2014-09-13 20:18:00 +0530

    v5

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,25 @@
 // JavaScript Document
+$(function(){
+	$("#menu1").click(function(){
+		$(".image_panel").hide();
+		$(".image_panel1").show();
+		});
+	$("#menu2").click(function(){
+		$(".image_panel").hide();
+		$(".image_panel2").show();
+		});
+	$("#menu3").click(function(){
+		$(".image_panel").hide();
+		$(".image_panel3").show();
+		});
+	$("#menu4").click(function(){
+		$(".image_panel").hide();
+		$(".image_panel4").show();
+		});
+		
+	$(".image_panel li img").click(function(){
+		var text = $(this).attr("data");
+		$("#play_text").val(text);
+		});
+
+	});
